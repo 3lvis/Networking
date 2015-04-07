d4685edb51e85c60e171202e5005a310a4f3e41e
Elvis Nuñez <elvisnunez@me.com>
2015-04-07 23:50:23 +0200

    Improve tests

--- Tests/Tests/Tests.m
+++ Tests/Tests/Tests.m
@@ -15,8 +15,8 @@ static NSString * const BaseURL = @"http://httpbin.org";
     __block BOOL success = NO;
     Networking *networking = [[Networking alloc] initWithBaseURL:BaseURL];
     [networking GET:@"/get"
-         completion:^(id JSON, NSError *error) {
-             XCTAssertNotNil(JSON);
+         completion:^(NSDictionary *JSON, NSError *error) {
+             XCTAssertEqualObjects(JSON[@"url"], @"http://httpbin.org/get");
              XCTAssertNil(error);
              success = YES;
          }];
@@ -25,14 +25,17 @@ static NSString * const BaseURL = @"http://httpbin.org";
 
 - (void)testGetStubs
 {
-    [Networking stubGET:@"/stories" response:@{@"first_name" : @"Elvis"}];
+    [Networking stubGET:@"/stories" response:@{@"name" : @"Elvis"}];
 
+    __block BOOL success = NO;
     Networking *networking = [[Networking alloc] initWithBaseURL:BaseURL];
     [networking GET:@"/stories"
-         completion:^(id JSON, NSError *error) {
-             XCTAssertNotNil(JSON);
+         completion:^(NSDictionary *JSON, NSError *error) {
+             XCTAssertEqualObjects(JSON[@"name"], @"Elvis");
              XCTAssertNil(error);
+             success = YES;
          }];
+    XCTAssertTrue(success);
 }
 
 @end
