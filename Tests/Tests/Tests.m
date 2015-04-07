32b23f900da894ddd77b9f147e27464e4c1a8395
Elvis Nuñez <elvisnunez@me.com>
2015-04-07 16:55:58 +0200

    Add tets

--- Tests/Tests/Tests.m
+++ Tests/Tests/Tests.m
@@ -1,9 +1,24 @@
 @import XCTest;
 
+#import "Networking.h"
+
 @interface Tests : XCTestCase
 
 @end
 
 @implementation Tests
+
+- (void)testGet
+{
+    XCTestExpectation *expectation = [self expectationWithDescription:@"Networking expectations"];
+
+    Networking *networking = [[Networking alloc] initWithBaseURL:@"https://api-news.layervault.com/api/v2"];
+    [networking getPath:@"/stories" completion:^(id JSON, NSError *error) {
+        XCTAssertNotNil(JSON);
+        [expectation fulfill];
+    }];
+
+    [self waitForExpectationsWithTimeout:60.0f handler:nil];
+}
 
 @end
