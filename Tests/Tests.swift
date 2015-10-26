92c8592515049f80d48a0db256434e7d62d06e78
Elvis Nuñez <elvisnunez@me.com>
2015-10-26 15:54:41 +0100

    Add Basic Auth

--- Tests/Tests.swift
+++ Tests/Tests.swift
@@ -3,119 +3,121 @@ import XCTest
 
 class Tests: XCTestCase {
     let baseURL = "http://httpbin.org"
+}
 
-    func testGET() {
+// MARK: GET
+
+extension Tests {
+    func testSynchronousGET() {
         var success = false
-
         let networking = Networking(baseURL: baseURL)
         networking.GET("/get", completion: { JSON, error in
-            if let JSON = JSON as? [String : AnyObject] {
-                let url = JSON["url"] as! String
-                XCTAssertEqual(url, "http://httpbin.org/get")
-                success = true
-            }
+            success = true
         })
 
         XCTAssertTrue(success)
     }
 
-    func testGETWithInvalidPath() {
-        var success = false
+    func testGET() {
+        let networking = Networking(baseURL: baseURL)
+        networking.GET("/get", completion: { JSON, error in
+            let JSON = JSON as! [String : AnyObject]
+            let url = JSON["url"] as! String
+            XCTAssertEqual(url, "http://httpbin.org/get")
+        })
+    }
 
+    func testGETWithInvalidPath() {
         let networking = Networking(baseURL: baseURL)
         networking.GET("/invalidpath", completion: { JSON, error in
             if let JSON: AnyObject = JSON {
                 fatalError("JSON not nil: \(JSON)")
             } else {
-                if let error = error {
-                    XCTAssertNotNil(error)
-                    success = true
-                }
+                XCTAssertNotNil(error)
             }
         })
-
-        XCTAssertTrue(success)
     }
 
     func testGETStubs() {
-        var success = false
         let networking = Networking(baseURL: baseURL)
 
         networking.stubGET("/stories", response: ["name" : "Elvis"])
 
         networking.GET("/stories", completion: { JSON, error in
-            if let JSON = JSON as? [String : String] {
-                let value = JSON["name"]
-                XCTAssertEqual(value!, "Elvis")
-                success = true
-            }
+            let JSON = JSON as! [String : String]
+            let value = JSON["name"]
+            XCTAssertEqual(value!, "Elvis")
         })
-
-        XCTAssertTrue(success)
     }
 
     func testGETStubsUsingFile() {
-        var success = false
         let networking = Networking(baseURL: baseURL)
 
         networking.stubGET("/entries", fileName: "entries.json", bundle: NSBundle(forClass: self.classForKeyedArchiver!))
 
         networking.GET("/entries", completion: { JSON, error in
-            if let JSON = JSON as? [[String : AnyObject]] {
-                let entry = JSON[0]
-                let value = entry["title"] as! String
-                XCTAssertEqual(value, "Entry 1")
-                success = true
-            }
+            let JSON = JSON as! [[String : AnyObject]]
+            let entry = JSON[0]
+            let value = entry["title"] as! String
+            XCTAssertEqual(value, "Entry 1")
         })
-        
+    }
+}
+
+// MARK: POST
+
+extension Tests {
+    func testSynchronousPOST() {
+        var success = false
+        let networking = Networking(baseURL: baseURL)
+        networking.POST("/post", params: nil) { JSON, error in
+            success = true
+        }
+
         XCTAssertTrue(success)
     }
 
     func testPOST() {
-        var success = false
         let networking = Networking(baseURL: baseURL)
         networking.POST("/post", params: ["username":"jameson", "password":"password"]) { JSON, error in
-            if let JSON = JSON {
-                XCTAssertNotNil(JSON, "JSON not nil")
-                XCTAssertNil(error, "Error")
-                success = true
-            }
+            XCTAssertNotNil(JSON!, "JSON not nil")
+            XCTAssertNil(error, "Error")
         }
-
-        XCTAssertTrue(success)
     }
 
     func testPOSTWithIvalidPath() {
-        var success = false
         let networking = Networking(baseURL: baseURL)
         networking.POST("/posdddddt", params: ["username":"jameson", "password":"password"]) { JSON, error in
-            if let error = error {
-                XCTAssertNotNil(error)
-                XCTAssertNil(JSON)
-                success = true
-            }
+            XCTAssertNotNil(error)
+            XCTAssertNil(JSON)
         }
-
-        XCTAssertTrue(success)
     }
 
     func testPOSTStubs() {
-        var success = false
         let networking = Networking(baseURL: baseURL)
 
         networking.stubPOST("/story", response: [["name" : "Elvis"]])
 
         networking.POST("/story", params: ["username":"jameson", "password":"password"]) { JSON, error in
-            if let JSON = JSON as? [[String : String]] {
-                let value = JSON[0]["name"]
-                XCTAssertEqual(value!, "Elvis")
-                success = true
-            }
+            let JSON = JSON as! [[String : String]]
+            let value = JSON[0]["name"]
+            XCTAssertEqual(value!, "Elvis")
         }
-
-        XCTAssertTrue(success)
     }
+}
+
+extension Tests {
+    func testBasicAuth() {
+        let networking = Networking(baseURL: baseURL)
+        networking.autenticate("user", password: "passwd")
+        networking.GET("/basic-auth/user/passwd", completion: { JSON, error in
+            let JSON = JSON as! [String : AnyObject]
+            let user = JSON["user"] as! String
+            let authenticated = JSON["authenticated"] as! Bool
+            XCTAssertEqual(user, "user")
+            XCTAssertEqual(authenticated, true)
+        })
+   }
 
     func testURLForPath() {
         let networking = Networking(baseURL: baseURL)
