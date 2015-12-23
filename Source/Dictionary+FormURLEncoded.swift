66802aada95299cfb9c5e13e3b90dbc88237f6e8
Elvis Nuñez <elvisnunez@me.com>
2015-12-23 14:13:24 +0100

    Make public extension

--- Source/Dictionary+FormURLEncoded.swift
+++ Source/Dictionary+FormURLEncoded.swift
@@ -1,12 +1,10 @@
-extension Dictionary where Key: StringLiteralConvertible {
+public extension Dictionary where Key: StringLiteralConvertible {
     func formURLEncodedFormat() -> String {
-
         var converted = ""
         for (index, entry) in self.enumerate() {
             if index > 0 {
                 converted.appendContentsOf("&")
             }
-
             converted.appendContentsOf("\(entry.0)=\(entry.1)")
         }
 
