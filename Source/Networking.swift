665a28233f4fdf8a6cd00f7d57579ca0ec4f6532
Elvis Nuñez <elvisnunez@me.com>
2016-01-19 14:48:08 +0100

    Add NetworkingErrorType

--- Source/Networking.swift
+++ Source/Networking.swift
@@ -4,6 +4,15 @@ import JSON
 import NetworkActivityIndicator
 
 /**
+ Categorizes a networking error.
+ - `Client:` The 4xx class of status code is intended for cases in which the client seems to have erred.
+ - `Server:` Response status codes beginning with the digit "5" indicate cases in which the server is aware that it has erred or is incapable of performing the request.
+ */
+public enum NetworkingErrorType {
+    case Client(Int), Server(Int)
+}
+
+/**
  Provides the a bridge for configuring your Networking object with NSURLSessionConfiguration.
  - `Default:` This configuration type manages upload and download tasks using the default options.
  - `Ephemeral:` A configuration type that uses no persistent storage for caches, cookies, or credentials.
@@ -102,6 +111,14 @@ public class Networking {
         let destinationURL = cachesURL.URLByAppendingPathComponent(url.absoluteString)
 
         return destinationURL
+    }
+
+    public func networkingErrorType(error: NSError) -> NetworkingErrorType {
+        if error.code >= 400 && error.code < 500 {
+            return .Client(error.code)
+        } else {
+            return .Server(error.code)
+        }
     }
 }
 
