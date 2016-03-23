415a0017da2bd6f6eace6b9b415b347306f50598
Elvis Nuñez <elvisnunez@me.com>
2016-03-23 10:33:26 +0100

    Update helpers

--- Sources/NetworkActivityIndicator.swift
+++ Sources/NetworkActivityIndicator.swift
@@ -2,24 +2,24 @@
     import UIKit
 #endif
 
-public class NetworkActivityIndicator: NSObject {
+class NetworkActivityIndicator: NSObject {
 
     /**
      The shared instance.
      */
-    public static let sharedIndicator = NetworkActivityIndicator()
+    static let sharedIndicator = NetworkActivityIndicator()
 
     /**
      The number of activities in progress.
      */
-    internal var activitiesCount = 0
+    var activitiesCount = 0
 
     /**
      A Boolean value that turns an indicator of network activity on or off.
 
      Specify true if the app should show network activity and false if it should not. The default value is false. A spinning indicator in the status bar shows network activity. Multiple calls to visible cause an internal counter to take care of persisting the number of times this method has being called.
      */
-    public var visible: Bool = false {
+    var visible: Bool = false {
         didSet {
             if visible {
                 self.activitiesCount += 1
--- Sources/TestCheck.swift
+++ Sources/TestCheck.swift
@@ -1,11 +1,11 @@
 import Foundation
 
-@objc public class TestCheck: NSObject {
+struct TestCheck {
     /**
     Method to check wheter your on testing mode or not.
     - returns: A Bool, `true` if you're on testing mode, `false` if you're not.
     */
-    public static let isTesting: Bool = {
+    static let isTesting: Bool = {
         let enviroment = NSProcessInfo.processInfo().environment
         let serviceName = enviroment["XPC_SERVICE_NAME"]
         let injectBundle = enviroment["XCInjectBundle"]
