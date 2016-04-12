bdeb8107ea6ea12f0040db6fe5beb8c89d421f4d
Elvis Nuñez <elvisnunez@me.com>
2016-04-12 13:36:57 +0200

    Fix deprecation

--- Sources/Networking+Image.swift
+++ Sources/Networking+Image.swift
@@ -3,6 +3,28 @@
 import Foundation
 import UIKit
 
+extension String {
+    func encodeUTF8() -> String? {
+        if let _ = NSURL(string: self) {
+            return self
+        }
+
+        let optionalLastComponent = self.characters.split { $0 == "/" }.last
+        if let lastComponent = optionalLastComponent {
+            let lastComponentAsString = lastComponent.map { String($0) }.reduce("", combine: +)
+            if let rangeOfLastComponent = self.rangeOfString(lastComponentAsString) {
+                let stringWithoutLastComponent = self.substringToIndex(rangeOfLastComponent.startIndex)
+                if let lastComponentEncoded = lastComponentAsString.stringByAddingPercentEncodingWithAllowedCharacters(NSCharacterSet.alphanumericCharacterSet()) {
+                    let encodedString = stringWithoutLastComponent + lastComponentEncoded
+                    return encodedString
+                }
+            }
+        }
+
+        return nil;
+    }
+}
+
 public extension Networking {
     /**
      Downloads an image using the specified path.
@@ -10,7 +32,7 @@ public extension Networking {
      - parameter completion: A closure that gets called when the image download request is completed, it contains an `UIImage` object and a `NSError`.
      */
     public func downloadImage(fullPath path: String, completion: (image: UIImage?, error: NSError?) -> ()) {
-        let encodedPath = path.stringByAddingPercentEscapesUsingEncoding(NSUTF8StringEncoding)!
+        let encodedPath = path.encodeUTF8()!
         let fullURL = NSURL(string: encodedPath)!
         guard let url = NSURL(string: (fullURL.absoluteString as NSString).stringByReplacingOccurrencesOfString("/", withString: "-")),
             cachesURL = NSFileManager.defaultManager().URLsForDirectory(.CachesDirectory, inDomains: .UserDomainMask).first else { fatalError("Couldn't normalize url") }
