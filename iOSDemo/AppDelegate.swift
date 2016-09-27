5726e098d3bc204e1ae0ffad61596c078ec82961
Elvis Nuñez <elvisnunez@me.com>
2016-09-27 06:34:55 +0200

    Add empty demo app

---
+++ iOSDemo/AppDelegate.swift
@@ -0,0 +1,20 @@
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder {
+    var window: UIWindow?
+}
+
+extension AppDelegate: UIApplicationDelegate {
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+        self.window = UIWindow(frame: UIScreen.main.bounds)
+
+        let controller = OptionsController(nibName: nil, bundle: nil)
+        let navigationController = UINavigationController(rootViewController: controller)
+        self.window?.rootViewController = navigationController
+
+        self.window?.makeKeyAndVisible()
+
+        return true
+    }
+}
---
+++ iOSDemo/OptionsController.swift
@@ -0,0 +1,10 @@
+import UIKit
+
+class OptionsController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        self.view.backgroundColor = UIColor.red
+    }
+}
+
