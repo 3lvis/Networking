329524540c35a018a69f4869b1773b9e2a05acdb
Igor Ranieri Elland <igor@elland.me>
2016-10-06 12:50:13 +0200

    Make library compatible with app extensions

--- Sources/NetworkActivityIndicator.swift
+++ Sources/NetworkActivityIndicator.swift
@@ -6,17 +6,17 @@ import Foundation
 
 class NetworkActivityIndicator: NSObject {
 
-    /** 
+    /**
      The shared instance.
      */
     static let sharedIndicator = NetworkActivityIndicator()
 
-    /** 
+    /**
      The number of activities in progress.
      */
     var activitiesCount = 0
 
-    /** 
+    /**
      A Boolean value that turns an indicator of network activity on or off.
 
      Specify true if the app should show network activity and false if it should not. The default value is false. A spinning indicator in the status bar shows network activity. Multiple calls to visible cause an internal counter to take care of persisting the number of times this method has being called.
@@ -36,7 +36,9 @@ class NetworkActivityIndicator: NSObject {
             #if os(iOS)
                 let deadline = DispatchTime.now() + Double(Int64(0.5 * Double(NSEC_PER_SEC))) / Double(NSEC_PER_SEC)
                 DispatchQueue.main.asyncAfter(deadline: deadline) {
-                    UIApplication.shared.isNetworkActivityIndicatorVisible = (self.activitiesCount > 0)
+                    if UIApplication.responds(to: NSSelectorFromString("shared")) {
+                        (UIApplication.value(forKeyPath: "shared") as? UIApplication)?.isNetworkActivityIndicatorVisible = (self.activitiesCount > 0)
+                    }
                 }
             #endif
         }
