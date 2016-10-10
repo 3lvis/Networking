4d7b361ccc722779681a1e35ff1eb9196a59d7ca
Elvis Nuñez <elvisnunez@me.com>
2016-10-10 15:57:14 +0200

    Refactor and make public

--- Sources/Dictionary+FormURLEncoded.swift
+++ Sources/Dictionary+FormURLEncoded.swift
@@ -1,17 +1,11 @@
 import Foundation
 
-extension Dictionary where Key: ExpressibleByStringLiteral {
+public extension Dictionary where Key: ExpressibleByStringLiteral {
 
-    func formURLEncodedFormat() -> String {
-        var converted = ""
-        for (index, entry) in self.enumerated() {
-            if index > 0 {
-                converted.append("&")
-            }
-            converted.append("\(entry.0)=\(entry.1)")
-        }
-
+    public func formURLEncodedFormat() -> String {
+        let converted = self.map { name, value in "\(name)=\(value)" }.joined(separator: "&")
         guard let encodedParameters = converted.addingPercentEncoding(withAllowedCharacters: CharacterSet.urlQueryAllowed) else { fatalError("Couldn't convert parameters to form url: \(converted)") }
+
         return encodedParameters
     }
 }
