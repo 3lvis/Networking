5aee16fa7318d4a8c25cbf4b7061422699730835
Elvis Nuñez <elvisnunez@me.com>
2016-12-06 15:40:41 +0100

    Encode plus sign

--- Sources/Dictionary+FormURLEncoded.swift
+++ Sources/Dictionary+FormURLEncoded.swift
@@ -6,9 +6,17 @@ public extension Dictionary where Key: ExpressibleByStringLiteral {
      Returns the parameters in using URL-enconding, for example ["username": "Michael", "age": 20] will become "username=Michael&age=20".
      */
     public func urlEncodedString() -> String {
-        let converted = self.map { key, value in "\(key)=\(value)" }.joined(separator: "&")
-        guard let encodedParameters = converted.addingPercentEncoding(withAllowedCharacters: CharacterSet.urlQueryAllowed) else { fatalError("Couldn't encode parameters: \(converted)") }
+        let keys = self.map { key, value -> String in
+            var encodedValue = "\(value)".addingPercentEncoding(withAllowedCharacters: .urlQueryAllowed)
+            encodedValue = encodedValue?.replacingOccurrences(of: "+", with: "%2B")
+            if let encodedValue = encodedValue {
+                return "\(key)=\(encodedValue)"
+            } else {
+                fatalError("Couldn't encode \(value)")
+            }
+        }
+        let converted = keys.joined(separator: "&")
 
-        return encodedParameters
+        return converted
     }
 }
