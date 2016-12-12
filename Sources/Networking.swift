24a93c65933e7905da8010b11cd60ab2fea1f978
Elvis Nuñez <elvisnunez@me.com>
2016-12-12 18:58:20 +0100

    Run SwiftFormat

--- Sources/Networking.swift
+++ Sources/Networking.swift
@@ -10,15 +10,15 @@ public extension Int {
         switch self {
         case URLError.cancelled.rawValue:
             return .cancelled
-        case 100..<200:
+        case 100 ..< 200:
             return .informational
-        case 200..<300:
+        case 200 ..< 300:
             return .successful
-        case 300..<400:
+        case 300 ..< 400:
             return .redirection
-        case 400..<500:
+        case 400 ..< 500:
             return .clientError
-        case 500..<600:
+        case 500 ..< 600:
             return .serverError
         default:
             return .unknown
