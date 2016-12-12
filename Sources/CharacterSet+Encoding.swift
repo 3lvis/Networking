a2d716007d00cd8f1255a59a41bdc2553c9555c8
Elvis Nuñez <elvisnunez@me.com>
2016-12-12 18:50:34 +0100

    Fix broken iOSDemo

--- Sources/CharacterSet+Encoding.swift
+++ Sources/CharacterSet+Encoding.swift
@@ -1,3 +1,5 @@
+import Foundation
+
 extension CharacterSet {
     static var urlQueryParametersAllowed: CharacterSet {
         // Does not include "?" or "/" due to RFC 3986 - Section 3.4
--- Sources/Deprecated.swift
+++ Sources/Deprecated.swift
@@ -1,3 +1,5 @@
+import Foundation
+
 public extension Networking {
 
     /**
