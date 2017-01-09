9f9fc10efb709bb325cbac0cf533c40f90f76a63
Elvis Nuñez <elvisnunez@me.com>
2017-01-09 15:20:03 +0100

    Test deprecated calls

---
+++ Tests/DeprecatedTests.swift
@@ -0,0 +1,158 @@
+import Foundation
+import XCTest
+
+class DeprecatedTests: XCTestCase {
+    let baseURL = "http://httpbin.org"
+
+    func testCancelGETWithPath() {
+        let expectation = self.expectation(description: "testCancelGET")
+
+        let networking = Networking(baseURL: baseURL)
+        networking.disableTestingMode = true
+        var completed = false
+        networking.GET("/get") { json, error in
+            XCTAssertTrue(completed)
+            XCTAssertEqual(error?.code, URLError.cancelled.rawValue)
+            expectation.fulfill()
+        }
+
+        networking.cancelGET("/get") {
+            completed = true
+        }
+
+        self.waitForExpectations(timeout: 15.0, handler: nil)
+    }
+
+    func testCancelGETWithID() {
+        let expectation = self.expectation(description: "testCancelGET")
+
+        let networking = Networking(baseURL: baseURL)
+        networking.disableTestingMode = true
+        var completed = false
+        let requestID = networking.GET("/get") { json, error in
+            XCTAssertTrue(completed)
+            XCTAssertEqual(error?.code, URLError.cancelled.rawValue)
+            expectation.fulfill()
+        }
+
+        networking.cancel(with: requestID) {
+            completed = true
+        }
+
+        self.waitForExpectations(timeout: 15.0, handler: nil)
+    }
+
+    func testCancelPOSTWithPath() {
+        let expectation = self.expectation(description: "testCancelPOST")
+
+        let networking = Networking(baseURL: baseURL)
+        networking.disableTestingMode = true
+        var completed = false
+        networking.POST("/post", parameters: ["username": "jameson", "password": "secret"]) { json, error in
+            XCTAssertTrue(completed)
+            XCTAssertEqual(error?.code, URLError.cancelled.rawValue)
+            expectation.fulfill()
+        }
+
+        networking.cancelPOST("/post") {
+            completed = true
+        }
+
+        self.waitForExpectations(timeout: 15.0, handler: nil)
+    }
+
+    func testCancelPOSTWithID() {
+        let expectation = self.expectation(description: "testCancelPOST")
+
+        let networking = Networking(baseURL: baseURL)
+        networking.disableTestingMode = true
+        var completed = false
+        let requestID = networking.POST("/post", parameters: ["username": "jameson", "password": "secret"]) { json, error in
+            XCTAssertTrue(completed)
+            XCTAssertEqual(error?.code, URLError.cancelled.rawValue)
+            expectation.fulfill()
+        }
+
+        networking.cancel(with: requestID) {
+            completed = true
+        }
+
+        self.waitForExpectations(timeout: 15.0, handler: nil)
+    }
+
+    func testCancelPUTWithPath() {
+        let expectation = self.expectation(description: "testCancelPUT")
+
+        let networking = Networking(baseURL: baseURL)
+        networking.disableTestingMode = true
+        var completed = false
+        networking.PUT("/put", parameters: ["username": "jameson", "password": "secret"]) { json, error in
+            XCTAssertTrue(completed)
+            XCTAssertEqual(error?.code, URLError.cancelled.rawValue)
+            expectation.fulfill()
+        }
+
+        networking.cancelPUT("/put") {
+            completed = true
+        }
+
+        self.waitForExpectations(timeout: 150.0, handler: nil)
+    }
+
+    func testCancelPUTWithID() {
+        let expectation = self.expectation(description: "testCancelPUT")
+
+        let networking = Networking(baseURL: baseURL)
+        networking.disableTestingMode = true
+        var completed = false
+        let requestID = networking.PUT("/put", parameters: ["username": "jameson", "password": "secret"]) { json, error in
+            XCTAssertTrue(completed)
+            XCTAssertEqual(error?.code, URLError.cancelled.rawValue)
+            expectation.fulfill()
+        }
+
+        networking.cancel(with: requestID) {
+            completed = true
+        }
+
+        self.waitForExpectations(timeout: 150.0, handler: nil)
+    }
+
+    func testCancelDELETEWithPath() {
+        let expectation = self.expectation(description: "testCancelDELETE")
+
+        let networking = Networking(baseURL: baseURL)
+        networking.disableTestingMode = true
+        var completed = false
+        networking.DELETE("/delete") { json, error in
+            XCTAssertTrue(completed)
+            XCTAssertEqual(error?.code, URLError.cancelled.rawValue)
+            expectation.fulfill()
+        }
+
+        networking.cancelDELETE("/delete") {
+            completed = true
+        }
+
+        self.waitForExpectations(timeout: 15.0, handler: nil)
+    }
+
+    func testCancelDELETEWithID() {
+        let expectation = self.expectation(description: "testCancelDELETE")
+
+        let networking = Networking(baseURL: baseURL)
+        networking.disableTestingMode = true
+        var completed = false
+        let requestID = networking.DELETE("/delete") { json, error in
+            XCTAssertTrue(completed)
+            XCTAssertEqual(error?.code, URLError.cancelled.rawValue)
+            expectation.fulfill()
+        }
+
+        networking.cancel(with: requestID) {
+            completed = true
+        }
+
+        self.waitForExpectations(timeout: 15.0, handler: nil)
+    }
+}
