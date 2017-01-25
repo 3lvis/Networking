953b9646b182a310eba0603b69d9b4f03a016f68
codeOfRobin <me@rmalhotra.com>
2017-01-26 02:40:46 +0530

    refactored parameter encoding

--- Sources/Dictionary+FormURLEncoded.swift
+++ Sources/Dictionary+FormURLEncoded.swift
@@ -6,22 +6,16 @@ public extension Dictionary where Key: ExpressibleByStringLiteral {
      Returns the parameters in using URL-enconding, for example ["username": "Michael", "age": 20] will become "username=Michael&age=20".
      */
     public func urlEncodedString() throws -> String {
-        var failedMessage: String?
-        let keys = self.map { key, value -> String in
-            let encodedValue = "\(value)".addingPercentEncoding(withAllowedCharacters: .urlQueryParametersAllowed)
-            if let encodedValue = encodedValue {
-                return "\(key)=\(encodedValue)"
-            } else {
-                failedMessage = "Couldn't encode \(value)"
-                return ""
-            }
-        }
+		
+		let pairs = try self.reduce([]) { current, kvPair -> [String] in
+			if let encodedValue = "\(kvPair.value)".addingPercentEncoding(withAllowedCharacters: .urlQueryParametersAllowed) {
+				return current + ["\(kvPair.key)=\(encodedValue)"]
+			} else {
+				throw NSError(domain: Networking.domain, code: 0, userInfo: [NSLocalizedDescriptionKey: "Couldn't encode \(kvPair.value)"])
+			}
+		}
 
-        if let failedMessage = failedMessage {
-            throw NSError(domain: Networking.domain, code: 0, userInfo: [NSLocalizedDescriptionKey: failedMessage])
-        }
-
-        let converted = keys.joined(separator: "&")
+        let converted = pairs.joined(separator: "&")
 
         return converted
     }
