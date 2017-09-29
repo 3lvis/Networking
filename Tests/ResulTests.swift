4d5b6c7cf01ab88f3ba6c9f725cf13c0d4833352
Elvis Nuñez <elvisnunez@me.com>
2017-09-29 22:41:20 +0200

    Add more tests

--- Tests/ResulTests.swift
+++ Tests/ResulTests.swift
@@ -15,21 +15,78 @@ class ResultTests: XCTestCase {
         switch result {
         case .success(let value):
             XCTAssertEqual(value.dictionaryBody.debugDescription, body.debugDescription)
+            XCTAssertEqual(value.arrayBody.debugDescription, [[String: Any]]().debugDescription)
+            XCTAssertEqual(value.data.hashValue, Data().hashValue)
+
+            switch value.json {
+            case .dictionary(let valueBody):
+                XCTAssertEqual(body.debugDescription, valueBody.debugDescription)
+            case .array(_), .data(_), .none:
+                XCTFail()
+            }
         case .failure(_):
             XCTFail()
         }
     }
 
     func testJSONResultArray() {
+        let body = [["a": "b"]]
+        let result = JSONResult(body: body, response: response, error: nil)
+        switch result {
+        case .success(let value):
+            XCTAssertEqual(value.dictionaryBody.debugDescription, [String: Any]().debugDescription)
+            XCTAssertEqual(value.arrayBody.debugDescription, body.debugDescription)
+            XCTAssertEqual(value.data.hashValue, Data().hashValue)
 
+            switch value.json {
+            case .array(let valueBody):
+                XCTAssertEqual(body.debugDescription, valueBody.debugDescription)
+            case .dictionary(_), .data(_), .none:
+                XCTFail()
+            }
+        case .failure(_):
+            XCTFail()
+        }
     }
 
     func testJSONResultData() {
+        let body = "Result".data(using: .utf8)!
+        let result = JSONResult(body: body, response: response, error: nil)
+        switch result {
+        case .success(let value):
+            XCTAssertEqual(value.dictionaryBody.debugDescription, [String: Any]().debugDescription)
+            XCTAssertEqual(value.arrayBody.debugDescription, [[String: Any]]().debugDescription)
+            XCTAssertEqual(value.data.hashValue, body.hashValue)
 
+            switch value.json {
+            case .data(let valueBody):
+                XCTAssertEqual(body.hashValue, valueBody.hashValue)
+            case .dictionary(_), .array(_), .none:
+                XCTFail()
+            }
+        case .failure(_):
+            XCTFail()
+        }
     }
 
     func testJSONResultNone() {
+        let body = "Invalid"
+        let result = JSONResult(body: body, response: response, error: nil)
+        switch result {
+        case .success(let value):
+            XCTAssertEqual(value.dictionaryBody.debugDescription, [String: Any]().debugDescription)
+            XCTAssertEqual(value.arrayBody.debugDescription, [[String: Any]]().debugDescription)
+            XCTAssertEqual(value.data.hashValue, Data().hashValue)
 
+            switch value.json {
+            case .dictionary(_), .array(_), .data(_):
+                XCTFail()
+            case .none:
+                break
+            }
+        case .failure(_):
+            XCTFail()
+        }
     }
 
     func testImageResultWithMalformedImage() {
