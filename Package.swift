887181a32483e6886220291d57f5ca0c0154acea
Elvis <elvisnunez@me.com>
2024-06-13 01:29:58 +0200

    Update minimum version

--- Package.swift
+++ Package.swift
@@ -6,7 +6,7 @@ import PackageDescription
 let package = Package(
     name: "Networking",
     platforms: [
-         .iOS(.v15), .macOS(.v12), .tvOS(.v15), .watchOS(.v8)
+        .iOS(.v16), .macOS(.v13), .tvOS(.v16), .watchOS(.v9)
     ],
     products: [
         .library(
